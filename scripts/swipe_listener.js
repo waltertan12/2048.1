bf07717ad5b2d2edd2d019fcf8fa6740a6114a91
Walter Tan <waltertan12@gmail.com>
2015-10-31 19:49:02 -0700

    Prevent scrolling on swipe

--- scripts/swipe_listener.js
+++ scripts/swipe_listener.js
@@ -1,7 +1,9 @@
 (function (root) {
+  var SWIPE_THRESHOLD_TIME = 200;
+
   var SwipeListener = root.SwipeListener = function () {
-    this.start = null;
-    this.end = null;
+    this.start = {x: 0, y: 0, time: Number.MAX_VALUE};
+    this.end = {x: 0, y: 0, time: Number.MAX_VALUE};
 
     root.addEventListener("touchstart", 
       function (e) {
@@ -9,33 +11,49 @@
         this.start = {x: t.clientX, y: t.clientY, time: Date.now() };
     }.bind(this), false);
 
+    root.addEventListener("touchmove", function(e) {
+      if (this.start.time - this.end.time < SWIPE_THRESHOLD_TIME) {
+        e.preventDefault();
+      }
+    }.bind(this), false);
+
     root.addEventListener("touchend", 
       function (e) {
+        e.preventDefault();
         var t = e.changedTouches[0]
         this.end = {x: t.clientX, y: t.clientY, time: Date.now() };
-        this.detectSwipe();
+        this.detectSwipe(e);
     }.bind(this), false);
   };
 
-  SwipeListener.prototype.detectSwipe = function () {
+  SwipeListener.prototype.reset = function () {
+    this.start = {x: 0, y: 0, time: Number.MAX_VALUE};
+    this.end = {x: 0, y: 0, time: Number.MAX_VALUE};
+  };
+
+  SwipeListener.prototype.detectSwipe = function (e) {
     var horizontalMove = this.end.x - this.start.x,
         verticalMove = this.end.y - this.start.y,
         time = this.end.time - this.start.time;
 
-    if (time < 150 && (Math.abs(horizontalMove) > 10 || Math.abs(verticalMove) > 10)) {
+    if (time < SWIPE_THRESHOLD_TIME && (Math.abs(horizontalMove) > 10 || Math.abs(verticalMove) > 10)) {
       this.findDirection({x: horizontalMove, y: verticalMove});
-    } else {
+      this.reset();
     }
   };
 
   SwipeListener.prototype.findDirection = function (move) {
     if (move.x > 0 && Math.abs(move.y) < 50) {
+      console.log("Swipe right");
       game.slide("up");
     } else if (move.x < 0 && Math.abs(move.y) < 50) {
+      console.log("Swipe left");
       game.slide("down");
     } else if (move.y < 0) {
+      console.log("Swipe up");
       game.slide("left");
     } else if (move.y > 0) {
+      console.log("Swipe down");
       game.slide("right");
     }
 
