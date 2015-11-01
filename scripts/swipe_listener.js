32da0c844cf883bba1ea2a134d25dbc1661e2518
Walter Tan <waltertan12@gmail.com>
2015-10-31 19:27:56 -0700

    Use changedTouches instead of touches

--- scripts/swipe_listener.js
+++ scripts/swipe_listener.js
@@ -5,15 +5,13 @@
 
     root.addEventListener("touchstart", 
       function (e) {
-        var t = e.touches[0]
-        console.log(e);
+        var t = e.changedTouches[0]
         this.start = {x: t.clientX, y: t.clientY, time: Date.now() };
     }.bind(this), false);
 
     root.addEventListener("touchend", 
       function (e) {
-        console.log(e);
-        var t = e.touches[0]
+        var t = e.changedTouches[0]
         this.end = {x: t.clientX, y: t.clientY, time: Date.now() };
         this.detectSwipe();
     }.bind(this), false);
@@ -25,25 +23,19 @@
         time = this.end.time - this.start.time;
 
     if (time < 150 && (Math.abs(horizontalMove) > 10 || Math.abs(verticalMove) > 10)) {
-      console.log("Swipe!");
       this.findDirection({x: horizontalMove, y: verticalMove});
     } else {
-      console.log("No Swipe!");
     }
   };
 
   SwipeListener.prototype.findDirection = function (move) {
     if (move.x > 0 && Math.abs(move.y) < 50) {
-      console.log("Swipe right");
       game.slide("up");
     } else if (move.x < 0 && Math.abs(move.y) < 50) {
-      console.log("Swipe left");
       game.slide("down");
     } else if (move.y < 0) {
-      console.log("Swipe up");
       game.slide("left");
     } else if (move.y > 0) {
-      console.log("Swipe down");
       game.slide("right");
     }
 
